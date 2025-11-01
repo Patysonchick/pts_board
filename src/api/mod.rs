a23778644dd5b06c04237ed2c871fb03d003ccf6
Эдгар Томский <edgarkozlov3229@gmail.com>
2025-11-01 22:29:33 +0300

    Optimized posting, added redirecting after posting.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,14 +5,18 @@ pub mod post;
 pub mod thread;
 
 pub enum Error {
-    DbErr, // (sea_orm::error::DbErr)
+    DbErr(sea_orm::error::DbErr),
     ThreadNotFound,
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         match self {
-            Error::DbErr => (StatusCode::INTERNAL_SERVER_ERROR, "Database error").into_response(),
+            Error::DbErr(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Database error\n{err}"),
+            )
+                .into_response(),
             Error::ThreadNotFound => (StatusCode::NOT_FOUND, "Thread not found").into_response(),
         }
     }
--- src/api/post.rs
+++ src/api/post.rs
@@ -2,9 +2,10 @@ use crate::entity::{post, thread};
 use crate::{AppState, api};
 use axum::Form;
 use axum::extract::State;
+use axum::response::Redirect;
 use chrono::Utc;
 use rand::Rng;
-use sea_orm::{ActiveModelTrait, DbErr, EntityTrait, Set, TransactionError, TransactionTrait};
+use sea_orm::{ActiveModelTrait, EntityTrait, Set, TransactionTrait};
 use serde::Deserialize;
 use serde_with::NoneAsEmptyString;
 use serde_with::serde_as;
@@ -29,58 +30,45 @@ const PASSWORD_LEN: usize = 22;
 pub async fn create(
     State(state): State<AppState>,
     Form(payload): Form<CreatePost>,
-) -> Result<String, api::Error> {
-    // TODO! сделать редирект
-    match state
-        .db
-        .transaction(|txn| {
-            Box::pin(async move {
-                let thread = thread::Entity::find_by_id(payload.thread_id)
-                    .one(txn)
-                    .await?;
+) -> Result<Redirect, api::Error> {
+    let txn = state.db.begin().await.map_err(api::Error::DbErr)?;
 
-                if thread == None {
-                    return Ok::<Result<post::Model, api::Error>, TransactionError<DbErr>>(Err(
-                        api::Error::ThreadNotFound,
-                    ));
-                }
-
-                let mut thread: thread::ActiveModel = thread.unwrap().into();
-                thread.bumped_at = Set(Utc::now().naive_utc());
-                thread.update(txn).await?;
-
-                let password: String = {
-                    let mut rng = rand::rng();
-                    (0..PASSWORD_LEN)
-                        .map(|_| {
-                            let idx = rng.random_range(0..CHARSET.len());
-                            char::from(CHARSET[idx])
-                        })
-                        .collect()
-                };
-
-                let post = post::ActiveModel {
-                    thread_id: Set(payload.thread_id),
-                    parent_id: Set(payload.parent_id),
-                    title: Set(payload.title),
-                    text: Set(payload.text),
-                    password: Set(password),
-                    ..Default::default()
-                };
-                Ok(Ok(post.insert(txn).await?))
-            })
-        })
+    let thread = thread::Entity::find_by_id(payload.thread_id)
+        .one(&txn)
         .await
-    {
-        Ok(post) => match post {
-            Ok(post) => {
-                let msg = format!("Created post, id - {}", post.id);
-                Ok(msg)
-            }
-            Err(err) => Err(err),
-        },
-        // TODO! сделать проверку на ошибки БД
-        Err(_err) => Err(api::Error::DbErr), // TODO! сделать возвращения в enum ошибки DbErr
-    }
-    // TODO! сделать редирект
+        .map_err(api::Error::DbErr)?;
+
+    let mut thread: thread::ActiveModel = thread.ok_or_else(|| api::Error::ThreadNotFound)?.into();
+    let time = Utc::now().naive_utc();
+
+    thread.bumped_at = Set(time);
+    thread.update(&txn).await.map_err(api::Error::DbErr)?;
+
+    let password = {
+        let mut rng = rand::rng();
+        (0..PASSWORD_LEN)
+            .map(|_| {
+                let idx = rng.random_range(0..CHARSET.len());
+                char::from(CHARSET[idx])
+            })
+            .collect()
+    };
+
+    let post = post::ActiveModel {
+        id: Default::default(),
+        thread_id: Set(payload.thread_id),
+        parent_id: Set(payload.parent_id),
+        title: Set(payload.title),
+        text: Set(payload.text),
+        created_at: Set(time),
+        password: Set(password),
+    };
+    let post = post.insert(&txn).await.map_err(api::Error::DbErr)?;
+
+    txn.commit().await.map_err(api::Error::DbErr)?;
+
+    tracing::info!("Created post №{} in a thread №{}", post.id, post.thread_id);
+
+    let redirect_url = format!("/thread/{}#post-{}", post.thread_id, post.id);
+    Ok(Redirect::to(&redirect_url))
 }
